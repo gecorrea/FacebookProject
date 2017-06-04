f728d187b09b33264fc687134ced7505d4989357
George E. Correa <george.e.correa@gmail.com>
2017-06-04 17:52:38 -0400

    Add login button.

--- FacebookProject/ViewController.swift
+++ FacebookProject/ViewController.swift
@@ -3,17 +3,15 @@ import FacebookLogin
 import FacebookShare
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+        loginButton.center = view.center
+        
+        view.addSubview(loginButton)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
